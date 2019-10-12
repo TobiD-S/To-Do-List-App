bb628218455b49a49918adab7e1d4ce947917d62
Tobi <delesafatobi@gmail.com>
2019-10-12 01:47:02 +0100

    Update app.js
    Core functionalities were added to the to-do list app.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ loadEvents();
 // loads every event in the page
 function loadEvents() {
     document.querySelector('form').addEventListener('submit', submit);
+    // recently added
+    document.getElementById('clear').addEventListener('click', clearList);
+    document.querySelector('ul').addEventListener('click', deleteOrTick);
 
 }
+
 // submits data function
 function submit(e) {
     e.preventDefault();
@@ -12,6 +16,7 @@ function submit(e) {
         addTask(input.value);
     input.value = '';
 }
+
 // add tasks
 function addTask(task) {
     let ul = document.querySelector('ul');
@@ -19,4 +24,38 @@ function addTask(task) {
     li.innerHTML = `<span class="delete">x</span><input type="checkbox"><label>${task}</label>`;
     ul.appendChild(li);
     document.querySelector('.tasksBoard').style.display = 'block';
+}
+
+// clear list
+function clearList(e) {
+    // setting the ul innerHTML to an empty string
+    let ul = document.querySelector('ul').innerHTML = '';
+}
+
+// delete tick
+function deleteOrTick(e) {
+    if (e.target.className == 'delete')
+        deleteTask(e);
+    else {
+        tickTask(e);
+    }
+}
+
+// delete task
+function deleteTask(e) {
+    let remove = e.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
+}
+
+// tick a task
+function tickTask(e) {
+    const task = e.target.nextSibling;
+    if (e.target.checked) {
+        task.style.textDecoration = "line-through";
+        task.style.color = "FF0000";
+    } else {
+        task.style.textDecoration = "none";
+        task.style.color = "#2F4F4F";
+    }
 }
